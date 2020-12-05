fe8b0fc40a61c173824a204470cc41ee4539f264
Kevin Gessner <kevin@kevingessner.com>
2020-12-04 19:39:31 -0500

    3.2

--- 3/sol.py
+++ 3/sol.py
@@ -2,15 +2,23 @@
 
 import sys
 
-def calc(s):
+def calc(s, dx, dy):
     x = 0
     trees = 0
-    for line in s.splitlines():
-        if line[x % len(line)] == '#':
-            trees += 1
-        x += 3
+    for i, line in enumerate(s.splitlines()):
+        if i % dy == 0:
+            if line[x % len(line)] == '#':
+                trees += 1
+            x += dx
     return trees
 
 if __name__ == '__main__':
-    print(calc(sys.stdin.read()))
+    s = sys.stdin.read()
+    prod = 1
+    for dx, dy in ((1,1), (3,1), (5,1), (7,1), (1,2)):
+    #for dx, dy in ((1,2),):
+        trees = calc(s, dx, dy)
+        prod *= trees
+        print(trees)
+    print(prod)
 
